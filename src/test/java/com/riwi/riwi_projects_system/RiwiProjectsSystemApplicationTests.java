a882a6f4bd328919b2b7759f29dac952c688d552
daguttt <dagutmu667@gmail.com>
2024-10-03 17:19:52 -0500

    chore: initial commit

---
+++ src/test/java/com/riwi/riwi_projects_system/RiwiProjectsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.riwi_projects_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiProjectsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
