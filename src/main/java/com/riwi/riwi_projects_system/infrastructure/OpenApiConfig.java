0866630aa20e38e172cd59f3cad76ca62058c942
daguttt <dagutmu667@gmail.com>
2024-10-03 18:19:47 -0500

    feat: add open api configuration

---
+++ src/main/java/com/riwi/riwi_projects_system/infrastructure/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.riwi.riwi_projects_system.infrastructure;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+  @Value("${server.port}")
+  private String serverPort;
+
+  @Value("${server.servlet.context-path}")
+  private String contextPath;
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    // Create a dynamic server URL based on the server port
+    String localServerUrl = String.format("http://localhost:%s%s", this.serverPort, this.contextPath);
+
+    // Define the server for OpenAPI
+    Server localServer = new Server().url(localServerUrl).description("Local Development Server");
+
+    Info info = new Info().title("Missing training API").version("1.0.0").description(
+        "REST API that allows teachers train students through learning missions and assigned skills.");
+    return new OpenAPI().servers(Collections.singletonList(localServer)).info(info);
+  }
+}
