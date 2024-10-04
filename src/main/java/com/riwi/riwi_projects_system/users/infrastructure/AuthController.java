2b5fbc852095e7a81a7570970cb48acd973b8e3b
Daniel Gutierrez Muñoz <70865733+daguttt@users.noreply.github.com>
2024-10-04 18:10:47 -0500

    feat: implement user registration (#6)

---
+++ src/main/java/com/riwi/riwi_projects_system/users/infrastructure/AuthController.java
@@ -0,0 +1,53 @@
+package com.riwi.riwi_projects_system.users.infrastructure;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riwi.riwi_projects_system.common.infrastructure.dtos.response.ProblemDetailWithErrors;
+import com.riwi.riwi_projects_system.users.application.AuthService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.riwi.riwi_projects_system.users.infrastructure.dtos.request.RegisterUserDto;
+import com.riwi.riwi_projects_system.users.infrastructure.dtos.response.RegisterResponseDto;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  @Autowired
+  private AuthService authService;
+
+  @Operation(summary = "Register user", description = "Description: Register a new user")
+  @ApiResponses(
+      value = {
+          @ApiResponse(responseCode = "201", description = "User registration is successful",
+              content = { @Content(mediaType = "application/json",
+                  schema = @Schema(implementation = RegisterResponseDto.class)) }),
+          @ApiResponse(responseCode = "400", description = "The request body has invalid values",
+              content = { @Content(mediaType = "application/json",
+                  schema = @Schema(implementation = ProblemDetailWithErrors.class)) }),
+          @ApiResponse(responseCode = "409", description = "User already exists",
+              content = { @Content(mediaType = "application/json",
+                  schema = @Schema(implementation = ProblemDetail.class)) }), })
+  @PostMapping("/register")
+  public ResponseEntity<RegisterResponseDto> registerUser(@Valid @RequestBody RegisterUserDto registerUserDto) {
+    authService.registerUser(registerUserDto);
+    RegisterResponseDto registerResponseDto = new RegisterResponseDto(HttpStatus.CREATED.value(),
+        "User registered successfully");
+    return ResponseEntity.status(HttpStatus.CREATED).body(registerResponseDto);
+  }
+
+}
