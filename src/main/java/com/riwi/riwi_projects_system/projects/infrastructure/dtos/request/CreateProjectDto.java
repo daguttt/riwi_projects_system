6c8f213d5ecc9ba78beb51f7fd95b8ef68bf1dd0
daguttt <dagutmu667@gmail.com>
2024-10-05 16:55:01 -0500

    feat: add validation for the `CreateProjectDto` 'tasks' array field (`CreateTaskDto`)

--- src/main/java/com/riwi/riwi_projects_system/projects/infrastructure/dtos/request/CreateProjectDto.java
+++ src/main/java/com/riwi/riwi_projects_system/projects/infrastructure/dtos/request/CreateProjectDto.java
@@ -1,7 +1,11 @@
 package com.riwi.riwi_projects_system.projects.infrastructure.dtos.request;
 
+import org.hibernate.validator.constraints.Length;
+
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,9 +20,14 @@ import lombok.ToString;
 @NoArgsConstructor
 public class CreateProjectDto {
     @Schema(example = "Project 1", description = "Name of the project")
-    @Size(min = 3, message = "Name must be at least 3 character")
+    @Length(min = 3, message = "Name must be at least 3 character")
     @NotBlank(message = "Name cannot be blank")
     private String name;
 
+    @Schema(exampleClasses = { CreateTaskDto.class },
+            description = "Tasks of the project")
+    @NotNull
+    @Size(min = 1, message = "At least one task is required")
+    @Valid
     private CreateTaskDto[] tasks;
 }
