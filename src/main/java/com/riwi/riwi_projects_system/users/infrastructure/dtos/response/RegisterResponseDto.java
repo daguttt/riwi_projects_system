3ae46958cd9707535d62b2e4497295fe6286161f
daguttt <dagutmu667@gmail.com>
2024-10-04 19:30:14 -0500

    fix: add `@SuperBuilder` to `RegisterResponseDto`

--- src/main/java/com/riwi/riwi_projects_system/users/infrastructure/dtos/response/RegisterResponseDto.java
+++ src/main/java/com/riwi/riwi_projects_system/users/infrastructure/dtos/response/RegisterResponseDto.java
@@ -2,6 +2,9 @@ package com.riwi.riwi_projects_system.users.infrastructure.dtos.response;
 
 import com.riwi.riwi_projects_system.common.infrastructure.dtos.response.ApiResponseDto;
 
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
 public class RegisterResponseDto extends ApiResponseDto {
   public RegisterResponseDto(int value, String string) {
     super(value, string);
