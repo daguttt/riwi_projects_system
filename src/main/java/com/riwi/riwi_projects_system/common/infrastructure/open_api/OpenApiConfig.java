5461b68b39745da05ee21fdae5d28b7f20006ffe
daguttt <dagutmu667@gmail.com>
2024-10-05 20:40:11 -0500

    feat: add security scheme for the API documentation

--- src/main/java/com/riwi/riwi_projects_system/common/infrastructure/open_api/OpenApiConfig.java
+++ src/main/java/com/riwi/riwi_projects_system/common/infrastructure/open_api/OpenApiConfig.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
@@ -28,6 +30,13 @@ public class OpenApiConfig {
 
     Info info = new Info().title("Riwi Projects System API").version("1.0.0")
         .description("REST API to manage projects and tasks.");
-    return new OpenAPI().servers(Collections.singletonList(localServer)).info(info);
+
+    // Define security scheme of the API
+    Components components = new Components().addSecuritySchemes("bearerAuth",
+        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
+            .bearerFormat("JWT").name("Authorization"));
+
+    return new OpenAPI().servers(Collections.singletonList(localServer)).info(info)
+        .components(components);
   }
 }
